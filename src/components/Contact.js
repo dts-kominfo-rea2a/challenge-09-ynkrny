9c44c66f8b248773f2a8524b5932adfc588fde55
ynkrny <yankurniya@gmail.com>
2022-06-19 13:47:55 +0700

    commit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,20 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from 'react';
+import '../App.css';
 
-const Contact = () => {
+const Contact = ({ data }) => {
     return (
-        <></>
+        <div className="contact-card">
+            <div className="contact-photo">
+                <img src={data.photo} alt="{data.name}" />
+            </div>
+            <div className="contact-detail">
+                <h1>{data.name}</h1>
+                <p>{data.phone}</p>
+                <p>{data.email}</p>
+            </div>
+        </div>
     )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,15 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
 import React from 'react';
+import '../App.css';
 
 const Header = () => {
     return (
-        <></>
-    )
-}
+        <div className="header">
+           <h1 >CALL A FRIEND</h1>
+           <p>your friendly contact app</p>
+           <p>*****************************************</p>
+        </div>
+    );
+};
 
 export default Header;
